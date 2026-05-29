07e85e7a648b3c4a66d56a09e509c045866f40ad
Obake89 <wojciech.likus@gmail.com>
2026-05-29 09:43:04 +0200

    new config

--- shared/config.js
+++ shared/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  brandName: "SchoolTools",
-  assignmentsApiUrl: "",
+  brandName: "PirxTools",
+  assignmentsApiUrl: "https://script.google.com/macros/s/AKfycbyzObHnJ-f3wa3kyeXaq2bwt2cfwPCYtJMotyD5beLIQ0vfzbY09ApD3T1W9k-vcrU/exec",
   theme: {
     accentColor: "#2563eb",
     accentStrongColor: "#1d4ed8",
