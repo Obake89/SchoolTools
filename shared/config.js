c54284152d0711144f7108c80bfffc8f2ce812e4
Obake89 <wojciech.likus@gmail.com>
2026-06-17 10:55:12 +0200

    rewr

--- shared/config.js
+++ shared/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   brandName: "PirxTools",
-  assignmentsApiUrl: "https://script.google.com/macros/s/AKfycbyoB8X_F5g4jswVtnnHO0gpp_3ob2kW80pUTli583H_8Nij60hushcDTuog-VAnXyli/exec",
+  assignmentsApiUrl: "https://script.google.com/macros/s/AKfycbwcDniI7DYwxt8jBwZ4Eev3W8eLl-rah6UvF9nI8yXoS9cZPhrfVI3KvRtGeeKbAxBN/exec",
   theme: {
     accentColor: "#2563eb",
     accentStrongColor: "#1d4ed8",
